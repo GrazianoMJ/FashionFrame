59c5ee45aed2c8d43b0697406ef36fb5ce48ba52
SilverSurfer <GrazianoMJ@users.noreply.github.com>
2017-07-25 16:20:10 -0400

    ColorPicker Version 1.1 Update - Class Inclusion Part 1
    Added a new file Initialize.txt for adding a list of names rather than hard coding
    Added the basic ColorCell & ColorPalette classes to simplify code
    Modified indentation with new "8 spaces" method to make compatible with EMACs for half of the file.
    Created new code to pull text from Initialize.txt to fill the palette name list
    Updated code that pulled data from the various palette .txt files to use the new ColorPalette and ColorCell classes

--- ColorPicker_Ver1.py
+++ ColorPicker_Ver1.py
@@ -1,57 +1,69 @@
 # Warframe Color Picker Program
 # Written by: Mike Graziano
-# Version: 1.0
+# Version: 1.1
+
+class ColorCell:
+        def __init__(self,red,green,blue):
+                self.red = red
+                self.green = green
+                self.blue = blue
+                self.hex = red*(16**4)+green*(16**2)+blue
+                self.RGBtext = "({0:d},{1:d},{2:d})".format(red,green,blue)
+                self.HEXtext = "0x{0:X}".format(self.hex)
+
+        def update(self,red,green,blue):
+                self.red = red
+                self.green = green
+                self.blue = blue
+                self.hex = red*(16**4)+green*(16**2)+blue
+                self.RGBtext = "({0:d},{1:d},{2:d})".format(red,green,blue)
+                self.HEXtext = "0x{0:X}".format(self.hex)
+
+class ColorPalette:
+        def __init__(self, name):
+                self.name = name
+                self.palette = [[ColorCell(0,0,0) for y in range(5)] for x in range(18)]
 
 """ Initialize major variables:
-    * file_list is a list that contains all the names of the standard Warframe pallettes.
-    * archive_pallette is a dictionary that will hold the Python representation of the Warframe pallettes.
-    * test_colors is a list that will hold dictionaries of the colors to be tested. """
-file_list = [
-    "Classic", "Classic_Saturated", "Daybreak", "Eximus", "Fire", "Grineer", "Halloween", "Ice", "Infested", "Lotus",
-    "Orokin", "Red_White_Blue", "Smoke", "Storm", "Tenno", "Twilight"
-]
+        * List object "file_list" holds all the names of the standard Warframe pallettes.
+        * Dictionary object "archive_pallette" holds the Python representation of the Warframe pallettes.
+        * List object "test_colors" holds dictionaries of the colors to be tested. """
+file_list = []
+with open("Initialize.txt", "r") as file:
+        file_size = int(file.readline().lstrip("Names =  ").rstrip("\n"))
+        for line in range(file_size):
+                file_list.append(file.readline().rstrip("\n"))
 archive_pallette = {}
 test_colors = []
 
 # Creates archive pallette dictionary from text files
 for item in file_list:
-    with open(item + ".txt", "r") as file:
-        full_pallette = []
-        for column in range(0, 5, 1):
-            partial_pallette = []
-            for row in range(0, 18, 1):
-                # Save line from open file
-                line_buffer = file.readline().rstrip("\n").split(",")
+        file_palette = ColorPalette(item)
+        with open(item + ".txt", "r") as file:
+                for column in range(0, 5, 1):
+                        for row in range(0, 18, 1):
+                                # Save line from open file
+                                line_buffer = file.readline().rstrip("\n").split(",")
 
-                # Error checking before proceeding to the integer initialization
-                if len(line_buffer) != 6:
-                    quit(print("Incorrect number of elements at cell {0:s},{1:s} in {2:s}".format(
-                        line_buffer[0], line_buffer[1], item
-                    )))
+                                # Error checking before proceeding to the integer initialization
+                                if len(line_buffer) != 6:
+                                        quit(print("Incorrect number of elements at cell {0:s},{1:s} in {2:s}".format(line_buffer[0], line_buffer[1], item)))
 
-                # Initializes everything but "HEX" value to integer
-                for index in range(len(line_buffer)):
-                    if index != len(line_buffer)-1:
-                        line_buffer[index] = int(line_buffer[index], 10)
+                                # Initializes everything but "HEX" value to integer
+                                for index in range(len(line_buffer)):
+                                        if index != len(line_buffer)-1:
+                                                line_buffer[index] = int(line_buffer[index], 10)
 
-                # Check that the RGB values & HEX value are equivalent
-                if int(line_buffer[5], 0) != line_buffer[2]*(16**4) + line_buffer[3]*(16**2) \
-                        + line_buffer[4]*(16**0):
-                    quit(print("Error located at cell {0:d},{1:d} in {2:s}".format(
-                        line_buffer[0], line_buffer[1], item
-                    )))
+                                # Check that the RGB values & HEX value are equivalent
+                                if int(line_buffer[5], 0) != line_buffer[2]*(16**4) + line_buffer[3]*(16**2) + line_buffer[4]*(16**0):
+                                        quit(print("Error located at cell {0:d},{1:d} in {2:s}".format(line_buffer[0], line_buffer[1], item)))
 
-                # Add values to the pallette matrix
-                partial_pallette.append({
-                    "Red": line_buffer[2],
-                    "Green": line_buffer[3],
-                    "Blue": line_buffer[4],
-                    "HEX": line_buffer[5]
-                })
-            full_pallette.append(partial_pallette)
-    archive_pallette.update({item: full_pallette})
+                                #Add values to the pallette matrix
+                                file_palette.palette[row][column].update(line_buffer[2],line_buffer[3],line_buffer[4])
 
-# Create test colors list from "Test_Input.txt" file
+        archive_pallette.update({item: file_palette})
+
+"""# Create test colors list from "Test_Input.txt" file
 with open("Test_Input.txt", "r") as file:
     for line in file:
         line_buffer = line.rstrip().split(",")
@@ -102,4 +114,6 @@ with open("Test_Output.txt", "w") as file:
 
         file.write("{0:s} can be duped with {1:s} ({2:d},{3:d})\n".format(
             color["Name"], approximation["Pallette"], approximation["Column"]+1, approximation["Row"]+1
-        ))
+        ))"""
+
+print("This program has completed its operation without issue.")
